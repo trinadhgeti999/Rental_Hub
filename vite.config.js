cb9125db815523f62db01de1e78f1d7614806133
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-04-27 21:59:28 +0530

    Fix vite.config.js for Laravel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
-
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
         hmr: {
-            overlay: false
-        }
+            overlay: false,
+        },
     },
-    // ... your other settings
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
